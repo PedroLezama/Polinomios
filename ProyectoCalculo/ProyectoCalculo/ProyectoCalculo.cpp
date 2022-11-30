0cb115fae9748b21fa5da8c9cf5c83f0b20dac82
PedroLezama <pedrolezama23@gmail.com>
2022-11-30 16:39:39 -0600

    Variables y menu del programa

--- ProyectoCalculo/ProyectoCalculo/ProyectoCalculo.cpp
+++ ProyectoCalculo/ProyectoCalculo/ProyectoCalculo.cpp
@@ -1,19 +1,30 @@
-// ProyectoCalculo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-
-int main()
+#include<stdio.h>
+#include <string.h>
+void main()
 {
-    std::cout << "Hello World!\n";
+	double polinomio1[50];
+	double polinomio2[50];
+	double x;
+	double poliresultado;
+	int opcion;
+
+	printf("1.- Resolver\n2.- Multiplicar\n3.- Dividir");
+	printf("\nQue desea hacer?: ");
+	scanf_s("%i",&opcion);
+
+	if (opcion == 1)
+	{
+		printf("Escribe el polinomio");
+	}
+	if (opcion == 2)
+	{
+		printf("Multiplicacion de polinomios");
+		printf("\nEscribe el primer polinomio");
+	}
+	if (opcion == 3)
+	{
+		printf("Division de polinomios");
+		printf("\nEscribe el primer polinomio");
+	}
+
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
