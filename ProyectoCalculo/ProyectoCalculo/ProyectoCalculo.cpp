5dfb573ad65aa6f9cc6b0010c6756f31f540250c
PedroLezama <pedrolezama23@gmail.com>
2022-11-30 18:35:50 -0600

    Se pregunta al usuario el numero de coeficientes y el valor de los mismos

--- ProyectoCalculo/ProyectoCalculo/ProyectoCalculo.cpp
+++ ProyectoCalculo/ProyectoCalculo/ProyectoCalculo.cpp
@@ -4,27 +4,41 @@ void main()
 {
 	double polinomio1[50];
 	double polinomio2[50];
+	int numerocoeficientes;
+	int coeficientes[10];
 	double x;
 	double poliresultado;
-	int opcion;
+	int opcion,i;
 
 	printf("1.- Resolver\n2.- Multiplicar\n3.- Dividir");
 	printf("\nQue desea hacer?: ");
 	scanf_s("%i",&opcion);
+	printf("Numero de coeficientes: ");
+	scanf_s("%i", &numerocoeficientes);
+	
+	for (i = 0; i < numerocoeficientes; i++)
+	{
+		printf("Coeficiente %i: ", i);
+		scanf_s("%i", &coeficientes[i]);
+		
+	}
 
 	if (opcion == 1)
 	{
-		printf("Escribe el polinomio");
+
+		printf("El valor de X para calcular: ");
 	}
 	if (opcion == 2)
 	{
 		printf("Multiplicacion de polinomios");
 		printf("\nEscribe el primer polinomio");
+
 	}
 	if (opcion == 3)
 	{
 		printf("Division de polinomios");
 		printf("\nEscribe el primer polinomio");
+
 	}
 
 }
